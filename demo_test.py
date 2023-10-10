e4ccd50529e90d63efb5eda9340f3a3e255d24bb
AustinFairyland <fairylandhost@outlook.com>
2023-10-10 15:22:26 +0800

    :art:demo

--- demo_test.py
+++ demo_test.py
@@ -22,10 +22,28 @@ from Bio import Entrez
 from Bio import Medline
 from datetime import timedelta
 
+from subprocess import call
+import pkg_resources
+
 
 def test():
     return True
- 
+
+
+def pip_update_all():
+    packages = [dist.project_name for dist in pkg_resources.working_set]
+    for dist in packages:
+        str_s = 'pip install --no-cache-dir --upgrade {}'.format(dist)
+        try:
+            print(str_s)
+            # call(str_s, shell=True)
+        except Exception as error:
+            print(error)
+            break
+
+    return True
+
 
 if __name__ == '__main__':
     print(test())
+    print(pip_update_all())
